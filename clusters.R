5eb3d0048b649daa6260b63657e55b8ce20110e9
amithshubhan <55019776+amithshubhan@users.noreply.github.com>
2022-11-21 10:22:07 +0530

    Update clusters.R

--- clusters.R
+++ clusters.R
@@ -32,3 +32,26 @@ fviz_cluster(km, data = df)
  
 # saving the file
 dev.off()
+
+
+#here
+
+library(ggplot2)
+library(ggfortify)
+library(dplyr)
+
+data = select(iris,c(1,4))
+wssplot <- function(data, nc=15, seed=1234){
+  wss <- (nrow(data)-1)*sum(apply(data,2,var))
+  for (i in 2:nc){
+    set.seed(seed)
+    wss[i] <- sum(kmeans(data, centers=i)$withinss)}
+  plot(1:nc, wss, type="b", xlab="Number of Clusters",
+       ylab="Within groups sum of squares")
+  wss
+}
+
+kmean = kmeans(data,2)
+kmean$centers
+autoplot(kmean,data,frame=TRUE)
+
