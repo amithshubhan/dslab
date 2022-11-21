515f22fb8762f489a159ece265de67dabb3d1599
amithshubhan <55019776+amithshubhan@users.noreply.github.com>
2022-11-21 10:27:31 +0530

    Update clusters.R

--- clusters.R
+++ clusters.R
@@ -55,3 +55,66 @@ kmean = kmeans(data,2)
 kmean$centers
 autoplot(kmean,data,frame=TRUE)
 
+
+
+#here 22
+
+
+
+install.packages("factoextra") 
+ install.packages("cluster") 
+  
+ library(factoextra) 
+ library(cluster) 
+  
+ df <- USArrests 
+  
+ df <- na.omit(df) 
+  
+ head(df) 
+ m <- c( "average", "single", "complete", "ward") 
+ names(m) <- c( "average", "single", "complete", "ward") 
+  
+ #function to compute agglomerative coefficient 
+ ac <- function(x) { 
+   agnes(df, method = x)$ac 
+ } 
+  
+ #calculate agglomerative coefficient for each clustering linkage method 
+ sapply(m, ac) 
+ #perform hierarchical clustering using Ward's minimum variance 
+ clust <- agnes(df, method = "ward") 
+  
+ #produce dendrogram 
+ pltree(clust, cex = 0.6, hang = -1, main = "Dendrogram") 
+  
+ #calculate gap statistic for each number of clusters (up to 10 clusters) 
+ gap_stat <- clusGap(df, FUN = hcut, nstart = 25, K.max = 10, B = 50) 
+  
+ #produce plot of clusters vs. gap statistic 
+ fviz_gap_stat(gap_stat) 
+  
+ #compute distance matrix 
+ d <- dist(df, method = "euclidean") 
+  
+ #perform hierarchical clustering using Ward's method 
+ final_clust <- hclust(d, method = "ward.D2" ) 
+  
+ #cut the dendrogram into 4 clusters 
+ groups <- cutree(final_clust, k=4) 
+  
+ #find number of observations in each cluster 
+ table(groups) 
+  
+ #append cluster labels to original data 
+ final_data <- cbind(USArrests, cluster = groups) 
+  
+ #display first six rows of final data 
+ head(final_data) 
+  
+ aggregate(final_data, by=list(cluster=final_data$cluster), mean)
+
+
+
+
+
